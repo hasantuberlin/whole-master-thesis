45ad91d3e0031ad46aa66a5a72b4d9fc574238ff
Md Kamrul Hasan <mdha19@big-dama-3.dima.tu-berlin.de>
2021-04-15 02:40:52 +0200

    table parsing

---
+++ parsing and prepre data/domainvicinitytableupdate_V_09_09-server.py
@@ -0,0 +1,204 @@
+########################################
+import os
+import re
+import io
+import sys
+import math
+import json
+import html
+import pickle
+import difflib
+import unicodedata
+import bs4
+import bz2
+import py7zr
+import numpy
+import mwparserfromhell
+import libarchive.public
+#import dataset
+import wikitextparser as wtp
+from pprint import pprint
+from wikitextparser import remove_markup, parse
+from pandas import DataFrame
+import html
+#from simplediff import diff as simplediff
+#from difflib_data import *
+########################################
+########################################
+class WikiErrorCorrection:
+    """
+    The main class.
+    """
+    def __init__(self):
+        """
+        The constructor.
+        """
+        #self.PRETRAINED_VALUE_BASED_MODELS_PATH = ""
+        #self.VALUE_ENCODINGS = ["identity", "unicode"]
+        #self.IGNORE_SIGN = "<<<IGNORE_THIS_VALUE>>>"
+        self.VERBOSE = True
+        #self.SAVE_RESULTS = True
+        #self.ONLINE_PHASE = False
+        #self.REFINE_PREDICTIONS = True
+        #self.LABELING_BUDGET = 20
+        #self.MAX_VALUE_LENGTH = 50
+        #self.REVISION_WINDOW_SIZE = 5
+
+    def make_list_from_sorted_json(self, revision_data_folder):
+        table_folder_count=0
+        unique_table_error_found=0
+        self.table_count_with_error=0
+        table_count_with_revision=0
+        rd_folder_path = revision_data_folder
+        modified_data_path=os.path.join('datasets','Table_07_09_for_creating_model')
+        if not os.path.isdir(modified_data_path):
+            os.mkdir(modified_data_path)
+        for folder in os.listdir(rd_folder_path): #initial archieve folder
+            if '04Sep' in folder:
+                print(folder)
+                rdd_folder_path=os.path.join(rd_folder_path,folder)
+                if os.path.isdir(os.path.join(rd_folder_path, folder)): #datasets/revision-data/archieve-foldername
+                    for nested_folder in os.listdir(os.path.join(rd_folder_path,folder)):
+                        print(nested_folder)
+                        revision_list=[]
+                        #print(nested_folder)
+                        page_folder=os.path.join(rdd_folder_path,nested_folder) #datasets/revision-data/archieve-foldername/page_folder
+                        #print(page_folder)
+                        table_No=1
+                        if os.path.isdir(os.path.join(rdd_folder_path, nested_folder)):
+                            for nested_nested_folder in os.listdir(os.path.join(rdd_folder_path,nested_folder)):
+                                table_folder_count=table_folder_count+1
+                                if os.path.isdir(os.path.join(page_folder, nested_nested_folder)):
+                                    ##########################
+                                      #Check the parent id and revision id
+                                    ##########################
+                                    filelist = os.listdir(os.path.join(page_folder, nested_nested_folder))
+                                    table_count_with_revision=table_count_with_revision+ len(filelist)
+                                    filelist = sorted(filelist,key=lambda x: int(os.path.splitext(x)[0].split('_')[1]))#sort the json file list with revision id
+                                    self.previous_revision_file=None
+                                    self.previous_json_name=None
+                                    for rf in filelist:
+                                        self.current_json_name=rf
+                                        current_parent_id=rf.split('.')[0].split('_')[0]
+                                        #print('current parent id', current_parent_id)
+                                        
+                                        #current_revision_id=rf.split('.')[0].split('_')[1]
+                                        #print(rf, 'parent_id:', current_parent_id, 'revision_id: ', current_revision_id)
+                                        if rf.endswith(".json"):                                      
+                                            self.current_revision_file=json.load(io.open(os.path.join(page_folder, nested_nested_folder, rf), encoding="utf-8"))
+                                            if self.previous_revision_file and self.previous_json_name:
+                                                #previous_parent_id= self.previous_json_name.split('.')[0].split('_')[0]
+                                                previous_revision_id= self.previous_json_name.split('.')[0].split('_')[1]
+                                                #print('previous_revision_id', previous_revision_id)
+                                                if previous_revision_id == current_parent_id:
+                                                    revision_list_from_fun=[]
+                                                    revision_list_from_fun=self.diff_check_revision()
+                                                    if revision_list_from_fun:
+                                                        revision_list.append(revision_list_from_fun)
+                                                    self.previous_revision_file=self.current_revision_file
+                                                    self.previous_json_name=self.current_json_name
+                                            else:
+                                                self.previous_revision_file= self.current_revision_file
+                                                self.previous_json_name=self.current_json_name
+                                    
+                                    if revision_list:
+                                        unique_table_error_found=unique_table_error_found+1
+                                        json.dump(revision_list, open(os.path.join(modified_data_path, nested_folder + '_' + str(table_No) + ".json"), "w", encoding='utf8'))
+                                        table_No=table_No+1
+                                           
+        print('Total table: ', table_folder_count, 'With revision: ', table_count_with_revision, 'Error Table: ', self.table_count_with_error)
+        txt_file="table_folder_count: "+ str(table_folder_count)+ ". With Revision: "+ str(table_count_with_revision) + "Error Table with revision: " + str(self.table_count_with_error) + "Unique table error found: " + str(unique_table_error_found)
+        with open("table_count_07_09.txt", "w") as text_file:
+            text_file.write(txt_file)
+    def diff_check_revision(self):
+        create_revision_list=[]
+        table_column_current=None
+        table_column_previous=None
+        code_current =mwparserfromhell.parse(self.current_revision_file[0], skip_style_tags=True)
+        code_previous=mwparserfromhell.parse(self.previous_revision_file[0], skip_style_tags=True)
+        try:
+            ########### Current revision table  data extraction
+            table1=code_current.filter_tags(matches=lambda node: node.tag=="table")
+            table_code_current = wtp.parse(str(table1[0])).tables[0]
+            table_data_current=table_code_current.data()
+            table_column_current=table_data_current[0]
+            ########## previous revision table data extraction
+            table2=code_previous.filter_tags(matches=lambda node: node.tag=="table")
+            table_code_previous = wtp.parse(str(table2[0])).tables[0]
+            table_data_previous=table_code_previous.data()
+            table_column_previous=table_data_previous[0]
+            df_data=DataFrame(table_data_previous)
+            header=df_data.iloc[0]
+            new_column_list=header.tolist()
+            df_data=df_data[1:]
+            df_data.columns=header
+        except Exception as e:
+            print('Exception from table data: ', str(e))
+        if table_column_current and table_column_previous and len(table_column_previous) == len(set(table_column_previous)):
+            self.table_count_with_error=self.table_count_with_error+1
+            if len(table_column_current)==len(table_column_previous):
+                text1=table_data_previous
+                text2=table_data_current 
+            
+                if text1 and text2:
+                    for index1, (txt1, txt2) in enumerate(zip(text1,text2)): #row parsing
+                        if index1==0:
+                            continue
+                        d = difflib.Differ()
+                        for index, (cell1,cell2) in enumerate(zip(txt1,txt2)): # values of row parsing
+                            create_revision_dict={}
+                            old_value=None
+                            new_value=None
+                            cell1=remove_markup(str(cell1))
+                            cell2=remove_markup(str(cell2))
+                            cell1=cell1.strip()
+                            cell2=cell2.strip()
+                            #print(cell1)
+                            #print(cell2)
+                            if cell1 and cell2 :
+                                diff1 = d.compare([''.join(cell1)], [cell2])
+                                try:
+                                    if diff1:
+                                        for line in diff1:
+                                            #print(line)
+                                            #print('###############################################################################')
+                                            if not line.startswith(' '):
+                                                if line.startswith('-'):
+                                                    old_value=line[1:]
+                                                if line.startswith('+'):
+                                                    new_value=line[1:]
+                                        if old_value and new_value:
+                                            #table_column_current1=remove_markup(str(table_column_current))
+                                            txt1=remove_markup(str(txt1))
+                                            old_value=remove_markup(str(old_value))
+                                            new_value=remove_markup(str(new_value))
+                                            column_name=new_column_list[index]
+                                            column_name=str(column_name)
+                                            #print(column_name)
+                                            #print(type(column_name))
+
+                                            column_values=df_data[column_name].tolist()
+                                            column_values=remove_markup(str(column_values))
+                                            #value = html.unescape(value)
+                                            #new_value = re.sub("[\t\n ]+", " ", new_value, re.UNICODE)
+                                            #value = value.strip("\t\n ")
+                                            cleanr = re.compile('<.*?>')
+                                            
+                                            all_column=list(df_data.columns)
+                                            #all_column=html.unescape(str(all_column))
+                                            #all_column=remove_markup(str(all_column))
+                                            all_column = re.sub(cleanr, ' ', str(all_column))
+                                            all_column=remove_markup(all_column)
+                                            column_name=re.sub(cleanr, ' ', str(column_name))
+                                            column_name=remove_markup(column_name)
+                                            if len(old_value)<50 and len(new_value)<50:
+                                                create_revision_dict={ "columns": all_column, "domain": column_values, "vicinity": txt1, "errored_column": column_name,"old_value": old_value, "new_value": new_value}
+                                                create_revision_list.append(create_revision_dict)
+                                                print('column: ',column_name,'old cell: ',old_value,  'new_cell: ', new_value)
+                                except Exception as e:
+                                    print('Exception from revised value: ', str(e))                 
+        return create_revision_list
+if __name__ == "__main__":
+    app = WikiErrorCorrection()
+    #app.extract_revisions(wikipedia_dumps_folder="datasets")
+    app.make_list_from_sorted_json(revision_data_folder="datasets/revision-data/04-09-2020")
---
+++ parsing and prepre data/tableparsingwithparentid.py
@@ -0,0 +1,208 @@
+########################################
+import os
+import re
+import io
+import sys
+import math
+import json
+import html
+import pickle
+import difflib
+import unicodedata
+import bs4
+import bz2
+import py7zr
+import numpy
+import mwparserfromhell
+import libarchive.public
+#import dataset
+import wikitextparser as wtp
+from pprint import pprint
+from wikitextparser import remove_markup, parse
+import datetime
+########################################
+########################################
+class WikiErrorCorrection:
+    """
+    The main class.
+    """
+    def __init__(self):
+        """
+        The constructor.
+        """
+        self.PRETRAINED_VALUE_BASED_MODELS_PATH = ""
+        self.VALUE_ENCODINGS = ["identity", "unicode"]
+        self.IGNORE_SIGN = "<<<IGNORE_THIS_VALUE>>>"
+        self.VERBOSE = True
+        self.SAVE_RESULTS = True
+        self.ONLINE_PHASE = False
+        self.REFINE_PREDICTIONS = True
+        self.LABELING_BUDGET = 20
+        self.MAX_VALUE_LENGTH = 50
+        self.REVISION_WINDOW_SIZE = 5
+             
+    def extract_revisions(self, wikipedia_dumps_folder):
+        """
+        This method takes the folder path of Wikipedia page revision history dumps and extracts infobox/table revision data.
+        """
+        self.rd_folder_path = os.path.join(wikipedia_dumps_folder, "revision-data")
+        if not os.path.exists(self.rd_folder_path):
+            os.mkdir(self.rd_folder_path)
+        compressed_dumps_list = [df for df in os.listdir(wikipedia_dumps_folder) if df.endswith(".7z")]
+        for file_name in compressed_dumps_list:
+            compressed_dump_file_path = os.path.join(wikipedia_dumps_folder, file_name)
+            dump_file_name, _ = os.path.splitext(os.path.basename(compressed_dump_file_path))
+            self.rdd_folder_path = os.path.join(self.rd_folder_path, dump_file_name)
+            if not os.path.exists(self.rdd_folder_path):
+                os.mkdir(self.rdd_folder_path)
+            else:
+                continue
+            archive = py7zr.SevenZipFile(compressed_dump_file_path, mode="r")
+            archive.extractall(path=wikipedia_dumps_folder)
+            archive.close()
+            decompressed_dump_file_path = os.path.join(wikipedia_dumps_folder, dump_file_name)
+            decompressed_dump_file = io.open(decompressed_dump_file_path, "r", encoding="utf-8")
+            page_text = ""
+            for i,line in enumerate(decompressed_dump_file):
+                line = line.strip()
+                if line == "<page>":
+                    page_text = ""
+                page_text += "\n" + line
+                if line == "</page>":
+                    page_tree = bs4.BeautifulSoup(page_text, "html.parser")
+                    self.page_folder=str(page_tree.id.text)
+                    #filelist1 = os.listdir('/content/drive/My Drive/datasets/revision-data/03-09-2020')
+                    #if int(self.page_folder)<=5030548 or sys.getsizeof(page_text)> 5000000:
+                    if int(self.page_folder)<=8420:
+                        #print('Page size: ', sys.getsizeof(page_text), ' byte')
+                        print(self.page_folder, ':Page already parsed')
+                        #print()
+                        continue
+                    else:
+                        print(self.page_folder, 'is processing now')
+                        print('Page size: ', sys.getsizeof(page_text), ' byte')
+                        print('Start Time', datetime.datetime.now())
+                        total_infobox_count=0
+                        total_table_count=0
+                        for revision_tag in page_tree.find_all("revision"):
+                            self.revision_id_parent="root"
+                            self.revision_id_current=revision_tag.find("id").text
+                            try:
+                                self.revision_id_parent=revision_tag.find("parentid").text
+                            except Exception as e:
+                                print('Exception: Parent Id: ', str(e))
+                            revision_text = revision_tag.find("text").text
+                            self.code =mwparserfromhell.parse(revision_text)
+                            self.table=self.code.filter_tags(matches=lambda node: node.tag=="table")
+                            revision_table_count=self.table_parsing()
+                            #revision_infobox_count=self.infobox_parsing()
+                            #total_infobox_count=total_infobox_count+revision_infobox_count
+                            total_table_count=total_table_count+revision_table_count
+                        #print("The total number of infobox in this page(with revision): {}".format(total_infobox_count))
+                        print(self.page_folder, ' of processing is finished')
+                        print('End time', datetime.datetime.now())
+                        print("The total number of table in this page(with revision): {}".format(total_table_count))            
+            decompressed_dump_file.close()
+            os.remove(decompressed_dump_file_path)
+    def infobox_parsing(self):
+        """
+        This method will extract all infobox templates with revision
+        """
+        infobox_count=0
+        templates = self.code.filter_templates()
+        for temp in templates:
+            json_list=[]
+            if "Infobox" in temp.name:
+                self.revision_page_folder_path=os.path.join(self.rdd_folder_path,self.page_folder)
+                if not os.path.exists(self.revision_page_folder_path):
+                    os.mkdir(self.revision_page_folder_path)
+                infobox_folder=remove_markup(str(temp.name))
+                infobox_folder= re.sub('[^a-zA-Z0-9\n\.]', ' ', (str(infobox_folder)).lower())
+                revision_infobox_folder_path=os.path.join(self.revision_page_folder_path,infobox_folder)
+                if not os.path.exists(revision_infobox_folder_path):
+                    os.mkdir(revision_infobox_folder_path)
+                json_list.append(str(temp))
+                json.dump(json_list, open(os.path.join(revision_infobox_folder_path, self.revision_id_parent + '_' + self.revision_id_current + ".json"), "w"))
+                print(temp.name)
+                infobox_count=infobox_count+1
+        return infobox_count
+    def table_parsing(self):
+        """
+        This method will extract all table templates with revision
+        """
+        table_count=0
+        if self.table:                       
+            for tebil in self.table:
+                json_list=[]
+                try:
+		    #TAG_RE=re.compile(r'<[^>]+>')
+			#TAG_RE.sub(' ', name)
+                    table_caption = wtp.parse(str(tebil)).tables[0].caption
+                    table_folder_name=remove_markup(str(table_caption))
+                    table_folder_name=table_folder_name.lower()
+                    table_folder_name=table_folder_name.strip()
+		    #table_folder_name=TAG_RE.sub(' ', table_folder_name)
+                except Exception as e:
+                    print('Exception: table folder name ', str(e))
+                    #print(str(e))
+                    continue   
+                
+                if table_caption:
+                  try:
+                      self.revision_page_folder_path=os.path.join(self.rdd_folder_path,self.page_folder)
+                      if not os.path.exists(self.revision_page_folder_path):
+                          os.mkdir(self.revision_page_folder_path)
+                      table_folder_name=table_folder_name.strip('\n')
+                      revision_table_folder_path=os.path.join(self.revision_page_folder_path,table_folder_name)
+                      revision_table_folder_path=revision_table_folder_path.strip()
+                      if not os.path.exists(revision_table_folder_path):
+                          os.mkdir(revision_table_folder_path)
+                  except Exception as e:
+                      print('Exception: revision table folder', str(e))
+                      continue
+                  table_count=table_count+1
+                  json_list.append(str(tebil))
+                  json.dump(json_list, open(os.path.join(revision_table_folder_path, self.revision_id_parent + '_' + self.revision_id_current + ".json"), "w"))
+                  print('Table caption: ', table_folder_name)
+                  table_count=table_count+1
+                                     
+        return table_count
+    def make_list_from_sorted_json(self, revision_data_folder):
+        rd_folder_path = revision_data_folder
+        list_from_sorted_json_path=os.path.join(revision_data_folder,'all_revision_list_per_table_or_infobox')
+        if not os.path.isdir(list_from_sorted_json_path):
+            os.mkdir(list_from_sorted_json_path)
+        for folder in os.listdir(rd_folder_path): #initial archieve folder
+            rdd_folder_path=os.path.join(rd_folder_path,folder)
+            if os.path.isdir(os.path.join(rd_folder_path, folder)): #datasets/revision-data/archieve-foldername
+                for nested_folder in os.listdir(os.path.join(rd_folder_path,folder)):
+                    print(nested_folder)
+                    page_folder=os.path.join(rdd_folder_path,nested_folder) #datasets/revision-data/archieve-foldername/page_folder
+                    print(page_folder)
+                    if os.path.isdir(os.path.join(rdd_folder_path, nested_folder)):
+                        for nested_nested_folder in os.listdir(os.path.join(rdd_folder_path,nested_folder)):
+                            revision_list=[]
+                            print(nested_nested_folder)
+                            if os.path.isdir(os.path.join(page_folder, nested_nested_folder)):
+                                filelist = os.listdir(os.path.join(page_folder, nested_nested_folder))
+                                filelist = sorted(filelist,key=lambda x: int(os.path.splitext(x)[0]))#sort the json file list
+                                for rf in filelist:
+                                    print(rf)
+                                    if rf.endswith(".json"):
+                                        try:
+                                            revision_list.append(json.load(io.open(os.path.join(page_folder, nested_nested_folder, rf), encoding="utf-8")))
+                                        except:
+                                            continue
+                                data_revision_list=json.dumps(revision_list)
+                                json.dump(data_revision_list, open(os.path.join(list_from_sorted_json_path, "%s.json") %nested_nested_folder, "w"))
+                            else:
+                                print('The path is not found')
+                    else:
+                        print('The path is not found')
+            else:
+                print('The path is not found')
+        
+if __name__ == "__main__":
+    app = WikiErrorCorrection()
+    app.extract_revisions(wikipedia_dumps_folder="datasets")
+    #app.make_list_from_sorted_json(revision_data_folder="datasets/revision-data")
